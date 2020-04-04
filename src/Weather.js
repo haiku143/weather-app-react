586ffd94b78346d08fcc9b878438d211dbc78ab6
haiku143 <59836602+haiku143@users.noreply.github.com>
2020-04-04 17:12:58 +0200

    basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -1,53 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(false);
   function handleResponse(response) {
     console.log(response.data);
+    setTemperature(response.data.main.temp);
+    setReady(true);
   }
 
-  const apiKey = "4d792cfd34dd05c4d245cf45fd0ef7bf";
-  let city = "New York";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city.."
-              className="form-control"
-            />
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
+        </form>
+        <h1>New York</h1>
+        <ul>
+          <li>Wednesday 10:00</li>
+          <li>Mostly Cloudy</li>
+        </ul>
+        <div classname="row mt-3">
+          <div className="col-6">
+            <div className="clearfix">
+              <img
+                src="http://openweathermap.org/img/wn/01d@2x.png"
+                alt="Sunny"
+                className="float-left"
+              />
+              <div className="float-left">
+                <span className="temperature">{Math.round(temperature)}</span>
+                <span className="unit">°C</span>
+              </div>
+            </div>
           </div>
-        </div>
-      </form>
-      <h1>New York</h1>
-      <ul>
-        <li>Wednesday 10:00</li>
-        <li>Mostly Cloudy</li>
-      </ul>
-      <div classname="row">
-        <div className="col-6">
-          <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="Sunny" />
-          6°C
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Preciptitation: 15%</li>
-            <li>Humidity: 72%</li>
-            <li>Wind: 13 km/h</li>
-          </ul>
+          <div className="col-6">
+            <ul>
+              <li>Preciptitation: 15%</li>
+              <li>Humidity: 72%</li>
+              <li>Wind: 13 km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "4d792cfd34dd05c4d245cf45fd0ef7bf";
+    let city = "New York";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
